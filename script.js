baa355cda2463b2f09d57381b0508fb15ad89c83
SuhanaShakir7 <busheskhanbushes3271@gmail.com>
2024-09-19 00:38:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// question no 1;
+alert("Suhana Shakir")
+
+
+//question no 2;
+var name = "hi "
+var studentname = "Suhana Shakir"
+
+alert(name + studentname);
+console.log(name + studentname);
+//question no  3;
+var name = "student"
+alert("hi my name" + "" + name + "" + "how are you");
+//question no 4;
+var value1 = "welcome ";
+var value2 = "to the   ";
+var value3 = " portal";
+var fullvalue = value1 + value2 + value3;
+alert(fullvalue);
+//question no5;
+
+var a = 2
+var b = 4
+var totalcost = (a++ + --b - --a + b++ + a + b);
+
+alert(totalcost);
+console.log(totalcost)
+//question no 6;
+var age = prompt("what is  you age");
+if (age > 18) {
+    alert("you are allowed")
+}
+else if (age < 17) {
+    alert("you are not allowed")
+}
+// question no 7;
+var timing = prompt("what is your class timing")
+if (timing > 12) {
+    alert("kindly come  on time")
+} 
+else if (timing < 11){
+    alert("well done")
+}
+else{
+    alert("you  are on time")
+}
+
